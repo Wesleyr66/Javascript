08d9b098df346359b5f09dd13f10c24bdb9d22f1
Wesley <wesley.rochao@hotmail.com>
2025-03-20 17:41:53 -0300

    initialize project with basic structure and configuration

--- testeintra/src/App.jsx
+++ testeintra/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function useLocalStorage(key, initialValue = '') {
+  const [value, setValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      // Se encontrou o item, tenta fazer o parse
+      return item ? JSON.parse(item) : initialValue;
+    } catch (e) {
+      console.error('Erro ao fazer parse do item no localStorage:', e);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      const serializedValue = JSON.stringify(value);
+      window.localStorage.setItem(key, serializedValue);
+    } catch (e) {
+      console.error('Erro ao salvar item no localStorage:', e);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+
+
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -22,6 +49,10 @@ function App() {
           count is {count}
         </button>
         <p>
+          <button onClick={() => setCount((count) => count - 1)}>
+            count is {count}
+          </button>
+          {count < 0 ? <p>salveeeee</p> : <p>oláaa</p>}
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
