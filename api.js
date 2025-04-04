db46be3d8bfb46f3a2a019b64e5119b4ac85b101
Wesley <wesley.rochao@hotmail.com>
2025-04-04 12:17:10 -0300

    Remove console logs and unused code in getApi function

--- api.js
+++ api.js
@@ -3,16 +3,12 @@ async function getApi() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users/')
     const data = await response.json();
 
-    console.log(data)
-
     let pessoa = {
       id : data.id,
       name : data.name,
       username : data.username,
       email : data.email
     }
-    console.log(JSON.stringify(pessoa))
-    document.body.innerHTML += JSON.stringify(pessoa)
 
     data.forEach((data) => {
       const obj = {
